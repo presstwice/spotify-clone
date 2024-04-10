73a5a5dff548e9c8a939a0de04cbd19377af3fea
presstwice <38543504+presstwice@users.noreply.github.com>
2024-04-10 10:42:18 +1000

    x

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Figtree } from "next/font/google";
+import "./globals.css";
+
+const figtree = Figtree({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Spotify Clone",
+  description: "Listen to music!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={figtree.className}>{children}</body>
+    </html>
+  );
+}
