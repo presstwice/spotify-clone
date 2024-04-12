6d93a6e7e3a7ac0442500dde2ca3c6fc3f5a4709
presstwice <38543504+presstwice@users.noreply.github.com>
2024-04-12 17:44:34 +1000

    Added play on hover to the liked songs component. Added Newest songs and list for songs

--- app/components/ListItem.tsx
+++ app/components/ListItem.tsx
@@ -2,6 +2,7 @@
 
 import { useRouter } from "next/navigation";
 import Image from "next/image";
+import { FaPlay } from "react-icons/fa";
 
 interface ListItemProps {
     image: string;
@@ -18,13 +19,16 @@ const ListItem: React.FC<ListItemProps> = ({image, name, href}) => {
     }
 
     return (
-        <button className="relative group flex items-center rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4">
+        <button onClick={onClick} className="relative group flex items-center rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4">
             <div className="relative min-h-[64px] min-w-[64px]">
                 <Image className="object-cover" fill src={image} alt="image"/>
             </div>
-            <p>
+            <p className="font-medium truncate py-5">
                 {name}
             </p>
+            <div className="absolute transition opacity-0 rounded-full flex items-center justify-center bg-green-500 p-4 drop-shadow-md right-5 group-hover:opacity-100 hover:scale-110">
+            < FaPlay className="text-black" />
+            </div>
         </button>
     );
 }
